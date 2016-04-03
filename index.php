1c4884d8afe6f8e949ae34147e1eb953d0a619e7
28kayak <kaya.ota@sjsu.edu>
2016-04-02 22:44:09 -0700

    edit

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 <!--<div class=right-side >
   <a href="signin.php"/>
 </div>  -->
-
+<a href="signin.php">Sign In/Sign Up</a>
+<a href="upload.php"> Upload </a>
 <h2>Recent Up</h2>
 <!-- top 3 most resent image uploaded -->
 <div class="container">
@@ -51,8 +52,92 @@
         </tr>
     </table>
 </div>
+
 <h2>Popularity</h2>
+
 <!-- top 10 most popular image -->
+<!-- <div class="container">
+    <table class="style-table"  align="center">
+        <tr>
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+
+                    </a>
+                    <div class="desc">
+                        <p>Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+                    </a>
+                    <div class="desc">
+                        <p>Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+                    </a>
+                    <div class="desc">
+                        <p> Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+        </tr>
+        <!--second line-
+        <tr>
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+
+                    </a>
+                    <div class="desc">
+                        <p>Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+                    </a>
+                    <div class="desc">
+                        <p>Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+                    </a>
+                    <div class="desc">
+                        <p> Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+        </tr>
+    </table>
+</div>
+
+
+
+
+-->
 <?php
 ?>
 
--- src/controllers/CreateDB.php
+++ src/controllers/CreateDB.php
@@ -6,24 +6,59 @@ $password = "password";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+
+
 
 
 
 mysqli_close($conn);
 
-function createDatabase($connection)
+function checkConnection($connection)
 {
-  // Create database
-  $sql = "CREATE DATABASE myDB";
-  if ($conn->query($sql) === TRUE) {
-      echo "Database created successfully";
-  } else {
-      echo "Error creating database: " . $conn->error;
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
   }
+  echo "Connected successfully";
+}
+
+function createUserInfo($connection)
+{
+  $sql = "CREATE TABLE User (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    password VARCHAR(50),
+    )";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table UserInfo created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 }//createDatabase
+
+function createImageTable($connection)
+{
+  $spl = "CREATE TABLE Image (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    caption VARCHAR(50),
+    date VARCHAR(50),
+    reg_date TIMESTAMP"
+
+}
+function createVotingTable($conncetion)
+{
+  $spl = "CREATE TABLE Image (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    rate INT(3) UNSIGNED,
+    username VARCHAR(30) NOT NULL,
+    filename VARCHAR(50) NOT NULL,
+    date VARCHAR(50),
+    reg_date TIMESTAMP"
+}
+
+
+
 ?>
