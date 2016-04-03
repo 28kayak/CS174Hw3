ba7d8f298fd7c0f1ab61d4e62fe1270a0a3ca357
28kayak <kaya.ota@sjsu.edu>
2016-04-03 12:13:04 -0700

    sigin in form

--- src/controllers/signin.php
+++ src/controllers/signin.php
@@ -1,7 +1,21 @@
-<form action="action_page.php">
-  First name:<br>
-  <input type="text" name="firstname" value="Mickey"><br>
-  Last name:<br>
-  <input type="text" name="lastname" value="Mouse"><br><br>
+<form action="../controllers/signin.php" method="post">
+  User Name:<br>
+  <input type="text" name="user" value="Enter User Name"><br>
+  Password:<br>
+  <input type="text" name="password" value="Enter password"><br><br>
   <input type="submit" value="Submit">
 </form>
+
+<?php
+
+$user = $_POST["user"];
+$password = $_POST["password"];
+echo "user name : ";
+echo $user."<br>";
+echo "password : ";
+echo $password."<br>";
+
+//call db and compare if user name and password exists already
+
+
+ ?>
