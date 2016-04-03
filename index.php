398556a82ce2d676084c89d6e3923af0eeeaad66
28kayak <kaya.ota@sjsu.edu>
2016-04-02 19:49:40 -0700

    init

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<head>
+   <link rel="stylesheet" type="text/css" href="index.css" media="all">
+</head>
+<html>
+<body>
+<h1>Image Rating</h1>
+<!--<div class=right-side >
+  <a href="signin.php"/>
+</div>  -->
+
+<h2>Recent Up</h2>
+<!-- top 3 most resent image uploaded -->
+<div class="container">
+    <table class="style-table"  align="center">
+        <tr>
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+
+                    </a>
+                    <div class="desc">
+                        <p>Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+                    </a>
+                    <div class="desc">
+                        <p>Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+            <td>
+                <div class="img">
+                    <a target="_blank" href="italian-landscape-mountains-nature.jpg">
+                        <img src="italian-landscape-mountains-nature.jpg" alt="Fjords" width="300" height="200">
+                    </a>
+                    <div class="desc">
+                        <p> Add a description of the image here</p>
+                    </div>
+                </div>
+            </td>
+
+        </tr>
+    </table>
+</div>
+<h2>Popularity</h2>
+<!-- top 10 most popular image -->
+<?php
+?>
+
+</body>
+</html>
+
+
+
+<?php
+
+
+
+
+
+
+ ?>
