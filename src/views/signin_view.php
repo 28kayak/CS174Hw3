5b30d942eb66dcda5a486a5180240b8775cf935f
28kayak <kaya.ota@sjsu.edu>
2016-04-04 01:01:07 -0700

    add one file

---
+++ src/views/signin_view.php
@@ -0,0 +1,13 @@
+<div>
+	<form id='login' action='../controllers/signin_control.php' method='post' >
+	<fieldset >
+		<legend>Login</legend>
+		<label for='username'>UserName:</label><br/>
+    	<input type='text' name='username' id='username' /><br/>
+    	<label for='password' >Password:</label><br/>
+    	<input type='password' name='password' id='password' maxlength="50" /><br/>
+
+	    <input type='submit' name='Submit' value='Submit' />
+	</fieldset>
+	</form>
+	</div>
